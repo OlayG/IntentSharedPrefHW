39c15c25e972ca675d382cf71cde17a0aea462e4
OlayG <ogureje@yahoo.com>
2019-07-02 10:37:32 -0400

    Initial commit

---
+++ app/src/main/java/com/noname/intentsharedprefhw/Person.java
@@ -0,0 +1,41 @@
+package com.noname.intentsharedprefhw;
+
+public class Person {
+
+    private String name, dob, aboutMe, favoritePet;
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getAboutMe() {
+        return aboutMe;
+    }
+
+    public void setAboutMe(String aboutMe) {
+        this.aboutMe = aboutMe;
+    }
+
+    public String getFavoritePet() {
+        return favoritePet;
+    }
+
+    public void setFavoritePet(String favoritePet) {
+        this.favoritePet = favoritePet;
+    }
+}
